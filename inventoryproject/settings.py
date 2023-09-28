2be911c05a598fda16ce830fe408781381245a04
E-digital1000 <techelvis7@gmail.com>
2023-09-28 01:32:53 +0000

    issues

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@ujr-e&a%8m%6!z(+ka16+(sm6cug(h6noe%#p%=6%d2nz5t+#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
