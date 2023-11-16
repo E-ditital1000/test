6c54eb67148beadbb0b513c1a4b73f64266d8b36
E-digital1000 <techelvis7@gmail.com>
2023-11-16 13:01:55 +0000

    Right

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -31,11 +31,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['a1connect.onrender.com', '.now.sh', '127.0.0.1', 'localhost']
 # Application definition
-#'jazzmin',
 
 INSTALLED_APPS = [
+    'jazzmin',
     'cloudinary_storage',
-    'admin_argon.apps.AdminArgonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -183,5 +182,21 @@ LOGIN_URL = 'user-login'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+JAZZMIN_SETTINGS = {
+    'site_header': "A1tech",
+    'site_brand': "Admin Panel",
+    'site_title': "A1tech Admin",
+    'site_logo': 'https://res.cloudinary.com/dkhobecps/image/upload/v1696708795/media/profile_images/logo_2_fmvthl.png',  # Path to your logo in STATIC_ROOT
+    'navigation_expanded': True,
+    'site_copyright': "© 2023 A1technical Engineering Solutions",
+    'show_powered_by': False,
+    'theme': 'light',
+    'show_ui_builder': True,
+    'JAZZMIN_UI_TWEAKS' :'dark_mode_theme',
+    'theme_color': '#FFA500',  # Custom theme color
+}
 
-
+JAZZMIN_UI_TWEAKS = {
+    "theme": "flatly",
+    "dark_mode_theme": "darkly",
+}
