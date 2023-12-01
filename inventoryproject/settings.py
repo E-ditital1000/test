5ba6c00e432985dd6f5f231466bd2b7b6570dd56
E-digital1000 <techelvis7@gmail.com>
2023-12-01 14:13:20 +0000

    uyt

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@ujr-e&a%8m%6!z(+ka16+(sm6cug(h6noe%#p%=6%d2nz5t+#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['a1connect.onrender.com', '.now.sh', '127.0.0.1', 'localhost']
 # Application definitions
